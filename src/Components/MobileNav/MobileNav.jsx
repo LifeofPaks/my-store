439720226f1469d2950f0f53cbe06ebf41cae0cc
Paks <promise.chukwudi@yahoo.com>
2023-08-11 06:17:59 +0100

    updated

---
+++ src/Components/MobileNav/MobileNav.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import "./MobileNav.scss";
+import Logo from "../../assets/images/logo.png";
+import ArrowDown from "../../assets/images/icons8-expand-arrow-52.png";
+import CartIcon from "../../assets/images/icons8-cart-50.png";
+import Search from "../../assets/images/icons8-search-50.png";
+import Favorite from "../../assets/images/icons8-favorite-50.png";
+import Flag from "../../assets/images/icons8-usa-flag-48.png";
+import Playstation from "../../assets/images/icons8-playstation-60.png";
+import Apple from "../../assets/images/icons8-apple-60.png";
+import { Link } from "react-router-dom";
+import Cart from "../Cart/Cart";
+import EmptyCart from "../EmptyCart/EmptyCart";
+
+const MobileNav = ({
+  toggleCart,
+  openCart,
+  cartStatus,
+  isEmpty,
+  exitEmptyCart,
+  homePage,
+}) => {
+  return (
+    <div className="mobileNav">
+      <div className="top">
+        <div className="center logo">
+          <Link to="/">
+            <img src={Logo} alt="logo" />
+            <h1>
+              PAKS <span>Everything</span>
+            </h1>
+          </Link>
+        </div>
+
+        <div className="menu">
+          <img
+            src="https://img.icons8.com/ios-glyphs/60/menu--v1.png"
+            alt="menu--v1"
+            className="hamburger"
+          />
+          <img
+            src="https://img.icons8.com/fluency-systems-regular/96/long-arrow-left.png"
+            alt="long-arrow-left"
+            className="arrow"
+          />
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="mnWrapper">
+          <Link to="/">
+            <img
+              src="https://img.icons8.com/ios/100/home--v1.png"
+              alt="home--v1"
+            />
+          </Link>
+
+          <div className="currency">
+            <div className="mnItem">
+              <img src={Flag} alt="flag" className="flag" />
+              <img src={ArrowDown} alt="arrow" className="arrow" />
+            </div>
+
+            <div className="mnItem">
+                <p>$</p>
+              <img src={ArrowDown} alt="arrow" className="arrow" />
+            </div>
+          </div>
+
+          <Link to="/products">
+            <img
+              src="https://img.icons8.com/glyph-neue/64/000000/multiple-devices.png"
+              alt="multiple-devices"
+              className="ps5"
+            />
+          </Link>
+
+          <Link to="playstation">
+            <img src={Playstation} alt="PS5" className="ps5" />
+          </Link>
+
+          <Link to="apple">
+            <img src={Apple} alt="apple" />
+          </Link>
+
+          <img src={Search} alt="Others" />
+          <img src={Favorite} alt="Others" />
+          <div className="cartWrapper" onClick={toggleCart}>
+            <img src={CartIcon} alt="Others" />
+            {!isEmpty && <span>1</span>}
+          </div>
+
+          {!isEmpty && openCart && (
+            <Cart cartStatus={cartStatus} isEmpty={isEmpty} />
+          )}
+          {isEmpty && (
+            <EmptyCart
+              exitEmptyCart={exitEmptyCart}
+              homePage={homePage}
+              isEmpty={isEmpty}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileNav;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,80 +7,105 @@ import Search from "../../assets/images/icons8-search-50.png";
 import User from "../../assets/images/icons8-user-32.png";
 import Favorite from "../../assets/images/icons8-favorite-50.png";
 import Flag from "../../assets/images/icons8-usa-flag-48.png";
-import PS5 from "../../assets/images/icons8-ps-5-48.png";
 import Playstation from "../../assets/images/icons8-playstation-60.png";
 import Apple from "../../assets/images/icons8-apple-60.png";
-import Others from "../../assets/images/icons8-multiple-devices-52.png";
 import { Link } from "react-router-dom";
 import Cart from "../Cart/Cart";
 import EmptyCart from "../EmptyCart/EmptyCart";
+import MobileNav from "../MobileNav/MobileNav";
 
-const Navbar = ({ toggleCart, openCart, cartStatus, isEmpty, exitEmptyCart, homePage }) => {
+const Navbar = ({
+  toggleCart,
+  openCart,
+  cartStatus,
+  isEmpty,
+  exitEmptyCart,
+  homePage,
+}) => {
   return (
-    <div className="navbar">
-      <div className="wrapper">
-        <div className="left">
-          <div className="item">
-            <img src={Flag} alt="flag" className="flag" />
-            <img src={ArrowDown} alt="arrow" className="arrow" />
+    <>
+      <MobileNav
+        toggleCart={toggleCart}
+        openCart={openCart}
+        cartStatus={cartStatus}
+        isEmpty={isEmpty}
+        exitEmptyCart={exitEmptyCart}
+        homePage={homePage}
+      />
+      <div className="navbar">
+        <div className="wrapper">
+          <div className="left">
+            <div className="item">
+              <img src={Flag} alt="flag" className="flag" />
+              <img src={ArrowDown} alt="arrow" className="arrow" />
+            </div>
+
+            <div className="item">
+              <span>USD</span>
+              <img src={ArrowDown} alt="arrow" className="arrow" />
+            </div>
+
+            <div className="item">
+              <Link to="/products">
+                <img
+                  src="https://img.icons8.com/glyph-neue/64/000000/multiple-devices.png"
+                  alt="multiple-devices"
+                  className="ps5"
+                />
+                Store
+              </Link>
+            </div>
+
+            <div className="item">
+              <Link to="playstation">
+                <img src={Playstation} alt="PS5" className="ps5" />
+                Play Station
+              </Link>
+            </div>
+
+            <div className="item">
+              <Link to="apple">
+                <img src={Apple} alt="apple" />
+                Apple
+              </Link>
+            </div>
           </div>
 
-          <div className="item">
-            <span>USD</span>
-            <img src={ArrowDown} alt="arrow" className="arrow" />
-          </div>
-
-          <div className="item">
-            <Link to="/products">
-            <img  src="https://img.icons8.com/glyph-neue/64/000000/multiple-devices.png" alt="multiple-devices" className="ps5"/>
-              Store
+          <div className="center logo">
+            <Link to="/">
+              <img src={Logo} alt="logo" />
+              <h1>
+                PAKS <span>Everything</span>
+              </h1>
             </Link>
           </div>
 
-          <div className="item">
-            <Link to="playstation">
-              <img src={Playstation} alt="PS5" className="ps5" />
-              Play Station
-            </Link>
-          </div>
+          <div className="right">
+            <Link to="/"> Home</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Contact</Link>
+            <img src={Search} alt="Others" />
+            <img src={User} alt="Others" />
+            <img src={Favorite} alt="Others" />
 
-          <div className="item">
-            <Link to="apple">
-              <img src={Apple} alt="apple" />
-              Apple
-            </Link>
-          </div>
-
-        </div>
-
-        <div className="center logo">
-          <Link to="/">
-            <img src={Logo} alt="logo" />
-            <h1>
-              PAKS <span>Everything</span>
-            </h1>
-          </Link>
-        </div>
-
-        <div className="right">
-          <Link to="/"> Home</Link>
-          <Link to="/">About</Link>
-          <Link to="/">Contact</Link>
-          <img src={Search} alt="Others" />
-          <img src={User} alt="Others" />
-          <img src={Favorite} alt="Others" />
-
-          <div className="cartWrapper" onClick={toggleCart}>
-            <img src={CartIcon} alt="Others" />
-            {!isEmpty && <span>1</span>}
+            <div className="cartWrapper" onClick={toggleCart}>
+              <img src={CartIcon} alt="Others" />
+              {!isEmpty && <span>1</span>}
+            </div>
           </div>
         </div>
+        {!isEmpty && openCart && (
+          <Cart cartStatus={cartStatus} isEmpty={isEmpty} />
+        )}
+        {isEmpty && (
+          <EmptyCart
+            exitEmptyCart={exitEmptyCart}
+            homePage={homePage}
+            isEmpty={isEmpty}
+          />
+        )}
       </div>
-      {!isEmpty && openCart && <Cart cartStatus={cartStatus} isEmpty={isEmpty} />}
-      {
-        isEmpty && <EmptyCart exitEmptyCart={exitEmptyCart} homePage={homePage} isEmpty={isEmpty}/>
-      }
-    </div>
+    </>
   );
 };
 
